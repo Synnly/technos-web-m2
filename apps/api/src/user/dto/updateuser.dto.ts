df7cff02a6669f5a12587cf6d580d98c064590a4
Synnly <emanuelfernandes54@gmail.com>
2025-10-16 18:03:06 +0200

    Tests dto

--- apps/api/src/user/dto/updateuser.dto.ts
+++ apps/api/src/user/dto/updateuser.dto.ts
@@ -1,12 +1,25 @@
-import { ArrayMaxSize, ArrayMinSize, IsDateString, IsPositive, IsString, IsStrongPassword } from "class-validator";
+import {
+	ArrayMaxSize,
+	ArrayMinSize,
+	IsArray,
+	IsDate,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+	IsStrongPassword,
+	Min,
+} from "class-validator";
+import { IsStringOrNull } from "../../../src/validators/isStringOrNull.validator";
 
 /**
  * DTO pour la mise à jour des informations d'un utilisateur.
  */
 export class UpdateUserDto {
 	@IsString()
+	@IsNotEmpty()
 	username: string;
 
+	@IsOptional()
 	@IsStrongPassword({
 		minLength: 8,
 		minUppercase: 1,
@@ -16,26 +29,42 @@ export class UpdateUserDto {
 	})
 	motDePasse?: string;
 
-	@IsPositive()
+	@IsOptional()
+	@Min(0)
 	points?: number;
 
-	@IsDateString()
+	@IsOptional()
+	@IsDate()
 	dateDerniereRecompenseQuotidienne?: Date | null;
 
+	@IsOptional()
 	@IsString({ each: true })
+	@IsNotEmpty({ each: true })
+	@IsArray()
 	predictions?: string[];
 
+	@IsOptional()
 	@IsString({ each: true })
+	@IsNotEmpty({ each: true })
+	@IsArray()
 	votes?: string[];
 
+	@IsOptional()
 	@IsString()
+	@IsNotEmpty()
 	role?: string;
 
+	@IsOptional()
 	@IsString({ each: true })
+	@IsNotEmpty({ each: true })
+	@IsArray()
 	cosmeticsOwned?: string[];
 
+	@IsOptional()
+	@IsArray()
 	@ArrayMaxSize(2)
 	@ArrayMinSize(2)
+	@IsStringOrNull({ each: true })
 	currentCosmetic?: (string | null)[];
 
 	constructor(partial: Partial<UpdateUserDto>) {
